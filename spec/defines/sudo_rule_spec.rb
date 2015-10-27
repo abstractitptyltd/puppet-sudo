87800a648e1c7a274d91ef919bf540b481a6fb5c
Pete Brown <pete@abstractit.com.au>
2015-10-27 15:57:00 +1000

    actually add spec tests this time

---
+++ spec/defines/sudo_rule_spec.rb
@@ -0,0 +1,46 @@
+#!/usr/bin/env rspec
+require 'spec_helper'
+
+describe 'sudo::rule', :type => :define do
+  on_supported_os({
+      :hardwaremodels => ['x86_64'],
+      :supported_os   => [
+        {
+          "operatingsystem" => "Ubuntu",
+          "operatingsystemrelease" => [
+            "14.04"
+          ]
+        },
+        {
+          "operatingsystem" => "CentOS",
+          "operatingsystemrelease" => [
+            "7"
+          ]
+        }
+      ],
+    }).each do |os, facts|
+    context "When on an #{os} system" do
+      let(:facts) do
+        facts.merge({
+          :concat_basedir => '/tmp',
+          :domain => 'domain.com',
+        })
+      end
+
+      context 'when fed no parameters' do
+        # let (:title) { 'my_cmnd'}
+        # let (:params) {{ 'what' => 'my_val','cmnd' => 'my_command', 'ensure' => 'present', 'comment' => 'blah blah'}}
+        # it 'should lay down our fact file as expected' do
+        #   should contain_file("#{facterbasepath}/facts.d/my_fact.yaml").with({
+        #     :path=>"#{facterbasepath}/facts.d/my_fact.yaml",
+        #     :ensure=>"present",
+        #     :owner=>"root",
+        #     :group=>"puppet",
+        #     :mode=>"0640"
+        #   }).with_content("# custom fact my_fact\n---\nmy_fact: \"my_val\"\n")
+        # end
+      end#no params
+
+    end
+  end
+end
